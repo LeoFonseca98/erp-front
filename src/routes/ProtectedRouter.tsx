8ed419466a38a531149e8df6352ed1952dbf8d55
LeoFonseca89 <leo-fonseca@outlook.com>
2026-04-27 11:05:12 -0300

    update new routes

--- src/routes/ProtectedRouter.tsx
+++ src/routes/ProtectedRouter.tsx
@@ -4,8 +4,6 @@ import { Navigate } from "react-router-dom";
 function ProtectedRouter({ children }: any) {
   const token = localStorage.getItem("token");
 
-  //console.log("ProtectedRoute rodando, token:", token);
-
   if (!token) {
     return <Navigate to="/login" />;
   }
