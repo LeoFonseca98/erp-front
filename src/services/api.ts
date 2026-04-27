23bfc5478b508e60e93ed94c29af941374b2e237
LeoFonseca89 <leo-fonseca@outlook.com>
2026-04-27 18:46:14 -0300

    Revert "Revert "teste2""
    This reverts commit 840a1e6d15150830a246348af873b8560676f859.

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "https://erp-back-6jmi.onrender.com",
-    //import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
